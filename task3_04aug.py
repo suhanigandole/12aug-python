3358115b57c75032d713f79b3d63d4b808abed63
suhanigandole <suhanigandole@gmail.com>
2026-08-12 22:44:02 +0530

    Delete task3_04aug.py

--- task3_04aug.py
+++
@@ -1,9 +0,0 @@
-# memory dectective
-V1 = 100
-V2 = 200 
-V3 = 100
-# find memory address
-
-print("Memory address of V1:", id(V1))
-print("Memory address of V2:", id(V2))
-print("Memory address of V3:", id(V3))
